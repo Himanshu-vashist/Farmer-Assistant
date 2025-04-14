08ceb9f364f9ff6ed82a730cb9ab53f17987e375
ujjawal-mukherjee <ujjawalkumarmukherjee335@gmail.com>
2025-04-15 02:19:17 +0530

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
-
-  config.resolver.assetExts.push("txt"); // Allow Metro to process .txt files
-
-  return config;
-})();
